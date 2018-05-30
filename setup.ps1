40bd81c19ed709a7da3713dee0729c11aac016b7
David Perfors <dnperfors@outlook.com>
2018-05-30 20:53:09 +0200

    Initial commit for setting up and modifying powershell.

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+﻿# Installing scoop as described: https://github.com/lukesampson/scoop
+set-executionpolicy remotesigned -s currentuser
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+scoop install 7zip git openssh concfg
+concfg import solarized-dark $PSScriptRoot\concfg\hack.json
