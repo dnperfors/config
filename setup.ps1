704fcd8a73b6f354d3caa04b40ee6d528082988f
David Perfors <dnperfors@outlook.com>
2018-05-30 20:58:16 +0200

    Install hack-font from extras

--- setup.ps1
+++ setup.ps1
@@ -2,5 +2,8 @@
 set-executionpolicy remotesigned -s currentuser
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
+scoop bucked add extras
+scoop install hack-font
+
 scoop install 7zip git openssh concfg
 concfg import solarized-dark $PSScriptRoot\concfg\hack.json
